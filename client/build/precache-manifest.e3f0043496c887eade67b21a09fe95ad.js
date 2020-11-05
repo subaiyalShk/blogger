7cdd97d029d15c552d6c258ab9061c60803f69c8
subaiyal <subaiyalshk@gmail.com>
2020-11-05 05:29:57 -0800

    build addedd'

---
+++ client/build/precache-manifest.e3f0043496c887eade67b21a09fe95ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1d059e0b5d8001f11ea",
+    "url": "/static/css/main.8c13fe98.chunk.css"
+  },
+  {
+    "revision": "b1d059e0b5d8001f11ea",
+    "url": "/static/js/main.bdc3431e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48b494f6a5ccad91e0d3",
+    "url": "/static/js/2.591e83aa.chunk.js"
+  },
+  {
+    "revision": "e30faf857f91b16c676731ad5458ed33",
+    "url": "/index.html"
+  }
+];
